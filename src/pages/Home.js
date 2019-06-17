630011be5cec682e0ff19d9ff0ba9f89c7cd649c
Wayne A. Tipton <wayne.tipton@gmail.com>
2019-06-17 13:39:58 -0500

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+export default function Home() {
+  return (
+    <Hero>
+      <Banner title="luxurious Rooms" subtitle="Deluxe Rooms Starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          our rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
+}
